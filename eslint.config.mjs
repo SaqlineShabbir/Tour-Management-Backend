174bd5dc29217e0adb4d30179c5690ea8ba8c519
SaqlineShabbir <saqline5869@gmail.com>
2025-08-16 23:23:15 -0700

    env setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,15 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-//   eslint.configs.recommended,
+  eslint.configs.recommended,
 //   tseslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
     rules:{
-        "no-console":"error"
+    //     "no-console":"error",
+    //      "no-unused-vars": "off",
+    // "@typescript-eslint/no-unused-vars": "error"
     }
   }
 );
