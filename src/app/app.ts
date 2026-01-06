5a924b874fa534927277eedb102288291f4467f9
SaqlineShabbir <saqline5869@gmail.com>
2026-01-06 20:53:02 +0600

    add: not found route added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,31 @@
-import express, { type NextFunction, type Request, type Response } from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import { userRoutes } from "./modules/user/user.route";
-import cors from "cors"
+import cors from "cors";
 import { success } from "zod";
 import { envVars } from "./config/env";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { router } from "./routes";
-const app = express()
+import notFoundMiddleware from "./middlewares/notFound";
 
-app.use(express.json())
-app.use(cors())
+const app = express();
 
-app.use("/api/v1",router)
+app.use(express.json());
+app.use(cors());
 
-app.get("/",(req:Request,res:Response)=>{
-    res.status(200).json({
-        message:"Welcome to Tour Managemet System"
-    })
-})
+app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Tour Managemet System",
+  });
+});
 
 // handle error globally
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
+app.use(notFoundMiddleware);
 
-export default app
+export default app;
