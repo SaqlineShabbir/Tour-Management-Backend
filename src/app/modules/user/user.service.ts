e1ae2ef316fdcad694e8ef075f26a4ef110e001e
SaqlineShabbir <saqline5869@gmail.com>
2026-01-09 13:04:31 +0600

    login added

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,13 +2,16 @@ import AppError from "../../errorHelpers/appError";
 import type { AuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
+import bcrypt from "bcryptjs";
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email,password, ...rest } = payload;
 
   const isExist = await User.findOne({ email });
   if (isExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "User already exists");
   }
+
+  const hashedPassword = await bcrypt.hash(password as string, 10);
 
   const authProvider : AuthProvider = {
     provider: "credentials",
@@ -17,6 +20,7 @@ const createUser = async (payload: Partial<IUser>) => {
 
   const user = await User.create({
     email: email,
+    password: hashedPassword,
     auths: [authProvider],
     ...rest,
   });
