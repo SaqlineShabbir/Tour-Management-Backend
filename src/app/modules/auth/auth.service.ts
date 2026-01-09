e08bab12ed177fdee76c3793e61127101da66ac8
SaqlineShabbir <saqline5869@gmail.com>
2026-01-09 19:32:36 +0600

    add: access token added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,11 +3,16 @@ import type { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const credintialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
-  console.log("Auth service called" , payload);
+  
 
-  const isExist = await User.findOne({ email });
+
+  const isExist = await User.findOne({ email }).select("+password");
+
+  console.log("isExist:", isExist);
 
   if (!isExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
@@ -18,14 +23,25 @@ const credintialsLogin = async (payload: Partial<IUser>) => {
     isExist.password as string
   );
 
+  console.log("isPasswordMatched:", isPasswordMatched);
+
   if (!isPasswordMatched) {
     throw new AppError(httpStatus.BAD_REQUEST, "Password does not match");
   }
 
   const { password: existedUserPassword, ...others } = isExist;
 
+  const jwtPayload = {
+    email: isExist.email,
+    role: isExist.role,
+    userId: isExist._id,
+  };
+  const accessToken = jwt.sign(jwtPayload, "your_jwt_secret_key", {
+    expiresIn: "1d",
+  });
+
   return {
-    ...others,
+    accessToken
   };
 };
 
