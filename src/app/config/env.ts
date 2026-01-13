1a0e9436fbf6f65726e301e13920afe504f17a30
SaqlineShabbir <saqline5869@gmail.com>
2026-01-13 10:57:51 +0600

    add: super admin seeding added

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,10 +8,12 @@ interface EnvConfig {
   JWT_SECRET: string;
   JWT_EXPIRES_IN: string;
   BCRYPT_SALT_ROUNDS: string;
+  SUPER_ADMIN_EMAIL:string
+  SUPER_ADMIN_PASSWORD:string
 }
 
 const loadEnvVars = (): EnvConfig => {
-  const requiredEnvVars: string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_SECRET", "JWT_EXPIRES_IN", "BCRYPT_SALT_ROUNDS"];
+  const requiredEnvVars: string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_SECRET", "JWT_EXPIRES_IN", "BCRYPT_SALT_ROUNDS","SUPER_ADMIN_EMAIL","SUPER_ADMIN_PASSWORD" ];
   requiredEnvVars.forEach((key) => {
     if (!process.env[key]) {
       throw new Error(`missing required env variables ${key}`);
@@ -24,6 +26,8 @@ const loadEnvVars = (): EnvConfig => {
     JWT_SECRET: process.env.JWT_SECRET as string,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string,
     BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS as string,
+    SUPER_ADMIN_PASSWORD:process.env.SUPER_ADMIN_PASSWORD as string,
+    SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string
   };
 };
 
