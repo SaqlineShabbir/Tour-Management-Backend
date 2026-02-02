b67757e8755fca69606942c7d3e9f2240c557a31
SaqlineShabbir <saqline5869@gmail.com>
2026-02-02 11:27:26 +0600

    zod error handled

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,7 +6,7 @@ export const globalErrorHandler = (
   err: any,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   let statusCode = 500;
   let message = `Something went wrong ! ${err.message}`;
@@ -14,26 +14,28 @@ export const globalErrorHandler = (
     const duplicate = err.message.match(/"([^"]*)"/);
     statusCode = 400;
     message = `${duplicate[1]} already exists ! Please use another email.`;
-  }
-  else if( err.name === "CastError" ) {
+  } else if (err.name === "CastError") {
     statusCode = 400;
     message = `Resource not found. Invalid: ${err.path}`;
-  }
-  else if (err.name === "ValidationError") {
+  } else if (err.name === "ValidationError") {
     statusCode = 400;
     message = Object.values(err.errors)
       .map((value: any) => value.message)
       .join(", ");
-  }
-  else if (err.name === "JsonWebTokenError") {
+  } else if (err.name === "JsonWebTokenError") {
     statusCode = 401;
     message = "Invalid token. Please log in again!";
-  }
-  else if (err.name === "TokenExpiredError") {
+  } else if (err.name === "TokenExpiredError") {
     statusCode = 401;
     message = "Your token has expired! Please log in again.";
-  }
-  else if (err instanceof AppError) {
+  } else if (err.name === "ZodError") {
+    statusCode = 400;
+    message = "zod error occurred";
+
+    err.issues.forEach((issue: any) => {
+      message += ` ${issue.path[0]}: ${issue.message}.`;
+    });
+  } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   } else if (err instanceof Error) {
